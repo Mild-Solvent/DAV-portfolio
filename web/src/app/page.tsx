85303265ba5e3e6dfac682aa27b18bb34c5de643
Mild-Solvent <matejcik.denis@gmail.com>
2025-10-10 02:09:09 +0300

    move sections

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
         background: 'transparent'
       }}>
         <About />
-        <Skills />
         <Projects />
+        <Skills />
         <Contact />
       </main>
     </>
